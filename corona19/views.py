e6af1b29c71e009193fddbc95630b07f862fb316
GeorgeTj <georgetj619@gmail.com>
2022-10-08 13:09:08 -0400

    Scrapping code add

--- corona19/views.py
+++ corona19/views.py
@@ -21,3 +21,43 @@ class Corona19ViewSet(viewsets.ModelViewSet):
 def coronadata_table_template(request):
     queryset = corona19_model.objects.all()
     return render(request, 'coronaDataTemplate.html', {'results': queryset})
+
+def webscrapper():
+    import requests
+    url = "https://www.worldometers.info/coronavirus/"
+    result = requests.get(url)
+    import bs4
+    soup = bs4.BeautifulSoup(result.text, 'lxml')
+    cases = soup.find_all('div', class_='maincounter-number')
+    data = []
+
+    for i in cases:
+        span = i.find('span')
+        data.append(span.string)
+
+    import pandas as pd
+    df = pd.DataFrame({'CoronaData': data})
+    df.index = ["TotalCases", "TotalDeaths", "TotalRecovered"]
+
+    requests.get('http://localhost:8000/corona_data/delete_all_data/')
+
+    import webbrowser
+    url = 'http://localhost:8000/corona_data/'
+    tc = df.iloc[0]
+    td = df.iloc[1]
+    tr = df.iloc[2]
+
+    df.iloc[0]
+    df.iloc[1]
+    df.iloc[2]
+
+    myobj = {
+        "totalcases": tc,
+        "totaldeaths": td,
+        "totalrecovered": tr
+    }
+
+    x = requests.post(url, data=myobj)
+
+    webbrowser.open(url)
+
